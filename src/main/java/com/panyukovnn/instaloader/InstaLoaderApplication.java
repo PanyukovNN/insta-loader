8a2eb140c20dd7cfe355e8299feb16e45539f11f
Nickolay <zylex77@gmail.com>
2021-08-04 23:53:49 +0300

    initial commit

---
+++ src/main/java/com/panyukovnn/instaloader/InstaLoaderApplication.java
@@ -0,0 +1,12 @@
+package com.panyukovnn.instaloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaLoaderApplication.class, args);
+    }
+}
